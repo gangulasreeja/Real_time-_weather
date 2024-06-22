46d8ed650cabd5b8d314a0288d658cfa80d5e60f
harshadeep-1008 <harsha123deep@gmail.com>
2024-06-22 23:01:00 +0530

    done 5th

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let cityInput = document.getElementById('city_input');
 let searchBtn = document.getElementById('searchBtn');
 let api_key = '9987a65804421b965b0279dd5ae4233c';
+let currentWeatherCard=document.querySelectorAll('.weather-left .card')[0];
+let fiveDaysForecastCard=document.querySelectorAll('.day-forecast')[0];
 function getWeatherDetails(name, lat, lon, country, state){
     FORECAST_API_URL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${api_key}`;
     WEATHER_API_URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${api_key}`;
@@ -10,12 +12,59 @@ function getWeatherDetails(name, lat, lon, country, state){
     fetch(WEATHER_API_URL)
         .then(response => response.json())
         .then(data => {
-            console.log(data);
-            // You can process the weather data here
+            let date=new Date();
+            currentWeatherCard.innerHTML=`
+            <div class="current-weather">
+              <div class="details">
+                <p>Now</p>
+                <h2>${(data.main.temp-273.15).toFixed(2)}&deg;C</h2>
+                <p>${data.weather[0].description}</p>    
+              </div>
+              <div class="weather-icon">
+                <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt="Weather Icon">
+
+              </div>
+
+            </div>
+            <hr>
+            <div class="card-footer">
+              <p><i class="fa-light fa-calendar"></i>${days[date.getDay()]},
+              ${date.getDate()},${months[date.getMonth()]}, ${date.getFullYear()}</p>
+              <p><i class="fa-light fa-location-dot"></i>${name},${country}</p>
+            </div>`;
         })
         .catch(() => {
             alert("Failed to fetch weather data");
         });
+        fetch(FORECAST_API_URL)
+        .then(response => response.json())
+        .then(data => {
+            let uniqueForecastDays=[];
+            let fiveDaysForecast = data.list.filter(forecast =>{
+                let forecastDate=new Date(forecast.dt_txt).getDate();
+                if(!uniqueForecastDays.includes(forecastDate)){
+                    return uniqueForecastDays.push(forecastDate);
+                }
+            }); 
+            fiveDaysForecastCard.innerHTML = '';
+            for (let i = 1; i < fiveDaysForecast.length; i++) {
+                let date = new Date(fiveDaysForecast[i].dt_txt);
+                fiveDaysForecastCard.innerHTML += `
+                <div class="forecast-item">
+                    <div class="icon-wrapper">
+                      <img src="https://openweathermap.org/img/wn/${fiveDaysForecast[i].weather[0].icon}.png" alt="">
+                      <span>${(fiveDaysForecast[i].main.temp - 273.15).toFixed(2)}&deg;C</span>
+                    </div>
+                    <p>${date.getDate()} ${months[date.getMonth()]}</p>
+                    <p>${days[date.getDay()]}</p>
+                </div>`;
+            }
+        })
+        .catch(() => {
+            alert("Failed to fetch forecast data");
+        });
+
+
 }
 function getCityCoordinates(){
     let cityName = cityInput.value.trim();
